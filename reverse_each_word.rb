f2d92dbeceade6aeb3d091efbfbeec62154a4f68
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-28 12:46:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  sentence.split.each do | word |
-    word.reverse
-  end
+  sentence.split.reverse
+  #each do | word |
+    #word.reverse
+  #end
   #sentence.reverse
 end
